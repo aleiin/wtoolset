fb35b36b38c65629e7377fd7822d983891b63035
vieiin <weibin@universeaction.com>
2023-11-24 11:03:23 +0800

    fix

--- lib/scan/scan_page_v2_args.dart
+++ lib/scan/scan_page_v2_args.dart
@@ -1,14 +1,14 @@
-// import 'package:mobile_scanner/mobile_scanner.dart';
-//
-// class ScanPageV2Args {
-//   const ScanPageV2Args({
-//     this.hint,
-//     this.onCallBack,
-//   });
-//
-//   /// 提示语
-//   final String? hint;
-//
-//   ///
-//   final Function(BarcodeCapture barcodeCapture)? onCallBack;
-// }
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+class ScanPageV2Args {
+  const ScanPageV2Args({
+    this.hint,
+    this.onCallBack,
+  });
+
+  /// 提示语
+  final String? hint;
+
+  ///
+  final Function(BarcodeCapture barcodeCapture)? onCallBack;
+}
